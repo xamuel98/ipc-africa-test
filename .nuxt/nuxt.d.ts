5e2527344942ec93e5721fe871600456be05ef37
xamuel98 <samuelakintomiwa98@gmail.com>
2025-11-22 10:55:58 +0100

    Initial commit

---
+++ .nuxt/nuxt.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="@nuxt/telemetry" />
+/// <reference types="@nuxt/devtools" />
+/// <reference path="types/builder-env.d.ts" />
+/// <reference types="nuxt" />
+/// <reference path="types/app-defaults.d.ts" />
+/// <reference path="types/plugins.d.ts" />
+/// <reference path="types/build.d.ts" />
+/// <reference path="types/schema.d.ts" />
+/// <reference path="types/app.config.d.ts" />
+/// <reference types="/Users/user/Documents/dev/NuxtProjects/ipc-africa-test/node_modules/@nuxt/nitro-server/dist/index.mjs" />
+/// <reference path="types/middleware.d.ts" />
+/// <reference path="types/components.d.ts" />
+/// <reference path="imports.d.ts" />
+/// <reference path="types/imports.d.ts" />
+/// <reference path="schema/nuxt.schema.d.ts" />
+/// <reference path="types/nitro.d.ts" />
+
+export {}
